febedd5c799b89520761c3befa2217074b454efc
JLemuel <79501195+JLemuel@users.noreply.github.com>
2024-07-31 16:54:32 +0800

    hero section and bento section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
     return (
@@ -21,10 +22,10 @@ const Hero = () => {
                 <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                     <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Dynamic Web Magic with Next.js</h2>
 
-                    <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl' words='Transforming concepts into seamless experiences' />
+                    <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl' words='Transforming concepts into Seamless User Experiences' />
                     <p className='text-center md:tracking-wider mb-4 text-sm md:te0xt-lg lg:text-2xl'>Hi, I&apos;m John</p>
                     <a href="#about">
-                        <MagicButton title="Show my Work" />
+                        <MagicButton title="Show my Work" icon={<FaLocationArrow />} position='right' />
                     </a>
                 </div>
             </div>
